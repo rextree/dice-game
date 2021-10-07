e6bf99b067867f9bec6d2e7b909226761866b5c8
Rex <rexyu24@gmail.com>
2021-10-08 00:23:47 +1300

    initial the game and styling

---
+++ script.js
@@ -0,0 +1,5 @@
+//generate the random number
+
+//apply the random number to left side and right side individually
+//compare the left side number and right side number
+//the larger side display win message
