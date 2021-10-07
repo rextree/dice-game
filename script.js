a6a2ee21c929c7198ff0dd77af715b0c4a2574b5
Rex <rexyu24@gmail.com>
2021-10-08 01:11:48 +1300

    Adding the function part
    add the function to let the dice roll and display the correct images
    change the html for better element targeting

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-//generate the random number
-
-//apply the random number to left side and right side individually
-//compare the left side number and right side number
-//the larger side display win message
+document.getElementById("play-button").onclick = () => {
+    //generate the random number function
+    const generateRandomNumber = () => Math.floor(Math.random() * 6 + 1);
+    //apply the random number to left side and right side individually
+    let leftNumber = generateRandomNumber();
+    let rightNumber = generateRandomNumber();
+    //display the correct dice image on each side according to the each side generated number
+    document
+        .querySelectorAll("img")[0]
+        .setAttribute("src", `./images/dice${leftNumber}.png`);
+    document
+        .querySelectorAll("img")[1]
+        .setAttribute("src", `./images/dice${rightNumber}.png`);
+    //compare the left side number and right side number, display the results accordingly
+    if (leftNumber > rightNumber) {
+        document.querySelector(
+            ".results"
+        ).innerHTML = `The left side is ${leftNumber} which is bigger than right side ${rightNumber}, Winner is <strong>LEFT SIDE</strong>.`;
+    } else if (rightNumber > leftNumber) {
+        document.querySelector(
+            ".results"
+        ).innerHTML = `The right side is ${rightNumber} which is bigger than left side ${leftNumber}, Winner is <strong>RIGHT SIDE</strong>.`;
+    } else if (leftNumber == rightNumber) {
+        document.querySelector(
+            ".results"
+        ).innerHTML = `Both sides are ${leftNumber}, you are <strong>DRAW</strong>. Roll the dice again.`;
+    }
+};
